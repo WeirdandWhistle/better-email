5ac9c06245f2a8fc1032e6736fa690b8c34f42e0
whynotjava <me@whynotjava.net>
2026-06-03 10:59:51 -0400

    add no caching to API responses

--- src/main/java/net/whynotjava/better_email/Util.java
+++ src/main/java/net/whynotjava/better_email/Util.java
@@ -3,19 +3,22 @@ package net.whynotjava.better_email;
 import java.nio.ByteBuffer;
 import java.util.UUID;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 
 public class Util {
-    public static final ResponseEntity<String> OKRes = new ResponseEntity<>("{\"status\":200,\"error\":\"ok\"}",HttpStatus.OK);
+    
+    public static final ResponseEntity<String> OKRes = new ResponseEntity<>("{\"status\":200,\"error\":\"ok\"}", noCache1(), HttpStatus.OK);
     public static ResponseEntity<String> error(int status, String error){
-        return new ResponseEntity<>("{\"status\":"+status+",\"error\":\""+error+"\"}",HttpStatusCode.valueOf(status));
+        return new ResponseEntity<>("{\"status\":"+status+",\"error\":\""+error+"\"}", noCache1(), HttpStatusCode.valueOf(status));
     }
     public static ResponseEntity<String> badRequest(String error){
-        return new ResponseEntity<>("{\"status\":400,\"error\":\""+error+"\"}",HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("{\"status\":400,\"error\":\""+error+"\"}", noCache1(), HttpStatus.BAD_REQUEST);
     }
-
+    public static HttpHeaders noCache1(){
+        final HttpHeaders noCache = new HttpHeaders();
+        noCache.add("Cache-Control", "no-store");
+        return noCache;
+    }
     public static byte[] convertUUIDToBytes(UUID uuid) {
         ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
         bb.putLong(uuid.getMostSignificantBits());
