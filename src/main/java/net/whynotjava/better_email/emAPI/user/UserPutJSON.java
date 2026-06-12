95faba56825ef3d57dbfa71ba2173be1264dbd53
whynotjava <me@whynotjava.net>
2026-06-12 11:50:41 -0400

    add signature and challenge + getters/setters

--- src/main/java/net/whynotjava/better_email/emAPI/user/UserPutJSON.java
+++ src/main/java/net/whynotjava/better_email/emAPI/user/UserPutJSON.java
@@ -1,9 +1,21 @@
 package net.whynotjava.better_email.emAPI.user;
 
+import java.util.HexFormat;
+
 public class UserPutJSON {
     private String type;
     private String value;
-    
+    private String signature;
+    private String challenge;
+    public String getChallenge() {
+        return challenge;
+    }
+    public void setChallenge(String challenge) {
+        this.challenge = challenge;
+    }
+    public byte[] getChallengeBytes(){
+        return HexFormat.of().parseHex(challenge);
+    }
     public String getType() {
         return type;
     }
@@ -15,5 +27,14 @@ public class UserPutJSON {
     }
     public void setValue(String value) {
         this.value = value;
+    }
+    public String getSignature() {
+        return signature;
+    }
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+    public byte[] getSignatureBytes(){
+        return HexFormat.of().parseHex(signature);
     }
 }
